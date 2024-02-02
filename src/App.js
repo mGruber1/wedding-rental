3c44ce4fb7f0e17dbdf6210499d7c029523e904e
Marcus Gruber <marcus-gruber@gmx.at>
2024-02-02 16:48:22 +0100

    refactor: refactored App.js for better readability

--- src/App.js
+++ src/App.js
@@ -1,50 +1,46 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HeroBanner from './components/HeroBanner/HeroBanner';
 import SearchBar from './components/SearchBar/SearchBar';
 import SearchResults from './components/SearchResults/SearchResults';
-import { useState } from 'react';
-
 
 function App() {
-
   const [searchValue, setSearchValue] = useState("");
   const [searchResults, setSearchResults] = useState([]);
 
-  const apiURL = process.env.REACT_APP_APIURL + "?title=" + searchValue;
+  const apiURL = `${process.env.REACT_APP_APIURL}?title=${searchValue}`;
 
-  const getSearchResults = (apiURL) => {
-    fetch(apiURL)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
+  const getSearchResults = async () => {
+    try {
+      const response = await fetch(apiURL);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+
+      if (searchValue.length !== 0) {
+        const filteredProducts = data.filter(product => product.title.includes(searchValue));
+        if (filteredProducts && filteredProducts.length > 0) {
+          setSearchResults(filteredProducts);
+          setSearchValue("");
         }
-        return response.json();
-      })
-      .then(data => {
-        if (searchValue.length !== 0) {
-          const filteredProducts = data.filter(product => product.title.includes(searchValue));
-          if (filteredProducts !== undefined) {
-            setSearchResults(filteredProducts);
-            setSearchValue("");
-          }
+      }
+    } catch (error) {
+      console.error('Fetch error:', error);
+    }
+  };
 
-        }
-      })
-      .catch(error => {
-        console.error('Fetch error:', error);
-      });
-
-  }
-
-  getSearchResults(apiURL);
+  useEffect(() => {
+    getSearchResults();
+  }, [searchValue, apiURL]);
 
   return (
     <div className="app">
-      <HeroBanner></HeroBanner>
-      <SearchBar setSearchValue={setSearchValue}></SearchBar>
-      <SearchResults searchResults={searchResults}></SearchResults>
-    </div >
+      <HeroBanner />
+      <SearchBar setSearchValue={setSearchValue} />
+      <SearchResults searchResults={searchResults} />
+    </div>
   );
 }
 
