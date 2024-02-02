b0a73399eeb0a028734e1aa981b574fa7e0f7cba
Marcus Gruber <marcus-gruber@gmx.at>
2024-02-02 10:39:33 +0100

    fix: fixed github problem

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SearchBar = () => {
+    return (
+        <div className="container mt-4 col-sm col-md-6 d-flex justify-content-center">
+            <form class="w-50">
+                <div className="form-group d-flex">
+                    <input type="text" class="form-control me-2" id="searchbox" placeholder="Search Wedding Rentals"></input>
+                    <button type="button" class="btn btn-primary">Search</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default SearchBar;
